80f6f31013cfcaf2dad3c2f9afda33f04078da00
Nicholas <65538312+nichoteloo@users.noreply.github.com>
2021-05-25 02:44:26 +0700

    Update consumer.py

--- djangoRT/firstpage/consumer.py
+++ djangoRT/firstpage/consumer.py
@@ -17,5 +17,20 @@ class DashConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self,text_data):
+        datapoint = json.loads(text_data)
+        val = datapoint['value']
+
+        await self.channel_layer.group_send(
+            self.groupname,
+            {
+                'type':'deprocessing',
+                'value':val
+            }
+        )
         print('>>>>',text_data)
-        pass 
+        #pass 
+    async def deprocessing(self,event):
+        valOther=event['value']
+        await self.send(text_data=json.dumps({'value':valOther}))
+        
+        
